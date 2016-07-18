0fe81e1b4adb46fb7ed0ba68a341fd31adc241fe
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-07-18 10:40:11 -0700

    add batchScoreDAO and StudentPoolDAO

--- src/main/groovy/edu/oregonstate/mist/recommendations/db/BatchScoreDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/recommendations/db/BatchScoreDAO.groovy
@@ -1,9 +1,27 @@
 package edu.oregonstate.mist.recommendations.db
 
+import org.skife.jdbi.v2.sqlobject.Bind
+import org.skife.jdbi.v2.sqlobject.SqlUpdate
+
 /**
  * BatchScore DAO
  */
 public interface BatchScoreDAO extends Closeable {
+
+    @SqlUpdate("""
+                INSERT INTO BATCH_SCORE (ID, STUDENT_POOL_ID, YEAR, MIN_SCORE)
+                SELECT SEQ_BATCH_SCORE.NEXTVAL, STUDENT_POOL.ID, '2012', 510
+                FROM STUDENT_POOL
+                WHERE PROVINCE = '浙江' AND
+                     STU_TYPE = '理科' AND
+                     BATCH = 1
+               """)
+    void insertBatchScore(@Bind("province") String province,
+                          @Bind("studentType") String studentType,
+                          @Bind("batch") Integer batch,
+                          @Bind("year") Integer year,
+                          @Bind("BatchScore") Integer batchScore)
+
     @Override
     void close()
 }
---
+++ src/main/groovy/edu/oregonstate/mist/recommendations/mapper/BatchScoreMapper.groovy
@@ -0,0 +1,7 @@
+package edu.oregonstate.mist.recommendations.mapper
+
+/**
+ * Created by shujinwu on 7/18/16.
+ */
+class BatchScoreMapper {
+}
