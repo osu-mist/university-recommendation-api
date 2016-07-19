85cbc2e960d51e27a126bfcc26ddc4c1b291a403
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-07-19 15:22:10 -0700

    modify to use notNUll annotation

--- src/main/groovy/edu/oregonstate/mist/recommendations/core/BatchScore.groovy
+++ src/main/groovy/edu/oregonstate/mist/recommendations/core/BatchScore.groovy
@@ -1,11 +1,21 @@
 package edu.oregonstate.mist.recommendations.core
 
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+
 /**
  * Batch Score Class
  */
 class BatchScore {
     Integer id
+
+    @NotNull
     StudentPool studentPool
+
+    @NotNull
     Integer year
+
+    @NotNull
     Integer minScore
 }
