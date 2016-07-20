007bcc7380605a790965f199700b43f31b147a55
Shujin Wu <shujinwu@onid.oregonstate.edu>
2016-07-19 17:06:14 -0700

    Revert "Feature/co 474 post batch scores"

--- src/main/groovy/edu/oregonstate/mist/recommendations/RecommendationConfiguration.groovy
+++
@@ -1,8 +0,0 @@
-package edu.oregonstate.mist.recommendations
-
-import edu.oregonstate.mist.api.Configuration
-import io.dropwizard.db.DataSourceFactory
-
-public class RecommendationConfiguration extends Configuration {
-    DataSourceFactory database = new DataSourceFactory()
-}
